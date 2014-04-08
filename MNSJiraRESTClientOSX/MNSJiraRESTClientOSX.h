e38ab6473ceb899807dc5ca464397943b23aef42
Erik Meissner <erik.meissner@prostep.com>
2014-04-08 12:14:02 +0200

    Added OSX-Target, local Podspec.

---
+++ MNSJiraRESTClientOSX/MNSJiraRESTClientOSX.h
@@ -0,0 +1,13 @@
+//
+//  MNSJiraRESTClientOSX.h
+//  MNSJiraRESTClientOSX
+//
+//  Created by Meissner, Erik on 08.04.14.
+//  Copyright (c) 2014 Medianet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MNSJiraRESTClientOSX : NSObject
+
+@end
---
+++ MNSJiraRESTClientOSX/MNSJiraRESTClientOSX.m
@@ -0,0 +1,13 @@
+//
+//  MNSJiraRESTClientOSX.m
+//  MNSJiraRESTClientOSX
+//
+//  Created by Meissner, Erik on 08.04.14.
+//  Copyright (c) 2014 Medianet. All rights reserved.
+//
+
+#import "MNSJiraRESTClientOSX.h"
+
+@implementation MNSJiraRESTClientOSX
+
+@end
